bcf2877a1711797d65e4985a14d341c32e4828ef
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-05-26 14:39:03 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import nbaLogo from '/SVG-nba.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  //create function to handle button click 
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={nbaLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className='button' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button className='button' onClick={() => setCount(0)}>reset</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/water.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+
+function water() {
+  return (
+    <div>water</div>
+  )
+}
+
+export default water
